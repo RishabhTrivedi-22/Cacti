185b9dacc59cf60ef06b1cbd0bf3628ab5054026
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-03-05 21:52:55 +0530

    Project Finished

--- main.js
+++ main.js
@@ -25,21 +25,58 @@ const linkAction = () => {
   // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove('show-menu');
 };
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
-const blurHeader = () =>{
-  const header = document.getElementById('header')
+const blurHeader = () => {
+  const header = document.getElementById('header');
   // Add a class if the bottom offset is greater than 50 of the viewport
-  this.scrollY >= 50 ? header.classList.add('blur-header') 
-                     : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+  this.scrollY >= 50 ? header.classList.add('blur-header')
+    : header.classList.remove('blur-header');
+};
+window.addEventListener('scroll', blurHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']');
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2500,
+  delay: 300,
+  // reset: true,
+
+});
+
+sr.reveal('.home_img, .new_data, .care_img, .contact_content, .footer');
+sr.reveal('.home_data, .care_list, .contact_img', { delay: 500 });
+sr.reveal('.new_card', { delay: 500, interval: 100 });
+sr.reveal('.shop_card', { interval: 100 });
